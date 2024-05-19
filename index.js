641785b213f84aca75241bf3c6ec25c8caea6412
mirazzaq <mirazzaq@gmail.com>
2024-05-19 18:17:05 +0500

    sequelize setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, (err) => {
+    if (err) {
+        process.exit(1);
+    }
+    console.log(`Server running on port ${port}`)
+})
